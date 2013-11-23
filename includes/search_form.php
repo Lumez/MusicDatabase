09d63310f75e66b1abc5180a797fe325cb71ec12
Stuart Kellaway <sk18g12@soton.ac.uk>
2013-11-23 17:32:31 +0000

    Created file search_form.php
    Here is the form for the search. This can be required in the areas
    needed

---
+++ includes/search_form.php
@@ -0,0 +1,4 @@
+<form name="search_form" method="post" action="searchresults.php">
+<input name="search" type="text" size="40" maxlength="60"/>
+<input type="submit" name="submit" value="Submit" />
+</form>
