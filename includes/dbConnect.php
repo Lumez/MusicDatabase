8bf8cda4d02a5817c6a142710f22ae30632c2a99
Ben <lumezious@googlemail.com>
2013-11-23 17:22:15 +0000

    Changed connection details
    Changed hostname, username, password and schema.

--- includes/dbConnect.php
+++ includes/dbConnect.php
@@ -1,9 +1,9 @@
 <?php
 
-$hostname = '';
-$username = '';
-$password = '';
-$schema = '';
+$hostname = 'linuxproj.ecs.soton.ac.uk';
+$username = 'gc1e12';
+$password = '4980';
+$schema = 'db_gc1e12';
 
 //database connect or kill script and display MySql error
 $connect = mysql_connect($hostname, $username, $password) or die (mysql_error());
