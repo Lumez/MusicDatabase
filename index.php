186fa4969e619a0ba78cee901f0972fe1bb7984f
Stuart Kellaway <sk18g12@soton.ac.uk>
2013-11-23 17:56:56 +0000

    Login System Added

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+
+
+<form action='login.php' method='POST'>
+	Username: <input type='text' name='username' /> <br />
+	Password: <input type='password' name='password' /> <br /><br />
+	<input type='submit' value='Submit' />
+</form>
+
+
+
+</html>
