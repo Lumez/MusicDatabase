b4160817d180a834d73f753a5156834846d6baf8
Ben <lumezious@googlemail.com>
2013-11-27 18:15:39 +0000

    Fixed the SQL queries in the add musician form

--- addMusician_result.php
+++ addMusician_result.php
@@ -12,17 +12,17 @@
     
         <?php 
                 try{
-                     if (empty($_GET['name']) OR empty($_GET['phoneNumber'])
+                    if (empty($_GET['name']) OR empty($_GET['phoneNumber'])
                          OR empty($_GET['str1']) OR empty($_GET['str2'])
                          OR empty($_GET['city']) OR empty($_GET['zipCode'])) throw new Exception('Incorrect Data types were included');
-                         {
-                            $d = $_GET['name'];
-                            $db->query("INSERT INTO address (phoneNo, Street1, Street2, city, 'Zip code') VALUES ('$_GET[phoneNumber]', '$_GET[str1]', '$_GET[str2])', '$_GET[city]), '$_GET[zipCode])'')");
-                            
-                            
-                            echo " $d The Musician and Address were added to the Database successfully :)";
-                         }
-                         
+
+                    $db->query("INSERT INTO address (phoneNo, Street1, Street2, city, `Zip code`) VALUES ({$_GET['phoneNumber']}, '{$_GET['str1']}', '{$_GET['str2']}', '{$_GET['city']}', '{$_GET['zipCode']}')");
+                    $db->query("INSERT INTO musician (name, phoneNo) VALUES ('{$_GET['name']}', '{$_GET['phoneNumber']}' )");
+
+                    "SELECT * FROM address WHERE phoneNo='$blah'"
+
+                    echo "{$_GET['name']} The Musician and Address were added to the Database successfully.";
+  
                  
                 }catch(Exception $e){
                              echo '<div class="alert alert-danger">';
