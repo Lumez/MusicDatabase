67da0b07685aefe64053a1b5cc7c5575e5acb494
Ben <lumezious@googlemail.com>
2013-12-01 20:03:43 +0000

    Changed search form to link to the correct place

--- includes/search_form.php
+++ includes/search_form.php
@@ -1,4 +1,4 @@
-<form name="search_form" method="post" action="searchresults.php">
+<form name="search_form" method="post" action="search.php">
 <input name="search" type="text" size="40" maxlength="60"/>
 <input type="submit" name="submit" value="Submit" />
 </form>
