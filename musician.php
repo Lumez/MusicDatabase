64998640c4f6a81bf2e89852e5ace7b92f093777
Ben <lumezious@googlemail.com>
2013-12-09 19:40:25 +0000

    Added my changes back to musician.php

--- musician.php
+++ musician.php
@@ -12,10 +12,6 @@
         <?php
             try {
                 if (!isset($_GET['id']) OR empty($_GET['id'])) throw new Exception("No musician selected!");
-                
-                $albumsAuthored = $db->query("SELECT * FROM album WHERE musicianID={$_GET['id']}");
-
-                $songsPerformed = $db->query("SELECT * FROM play_list WHERE musicianID={$_GET['id']}");
 
                 $musician = $db->query("SELECT name FROM musician WHERE musicianID={$_GET['id']}");
 
@@ -24,13 +20,67 @@
 
                 echo("<h1>$musicianName's Library</h1>");
 
-                //if there are more than 1 album, throw exception with error message saying library is empty
-                //if($songsPerformed->num_rows == 0) throw new Exception("Musician does not play any songs!");
-                //if($albumsAuthored->num_rows == 0) throw new Exception("Musician has not authored any albums!");
+            } catch (Exception $e) {
+                echo "<p>{$e->getMessage()}</p><a href=\"home.php\">Home</a>";
+            }
         ?>
                 <br />
+                <h2>Performed Songs</h2>
 
-                <h3><u>Authored Albums</u></h3>
+                <?php                 
+                    $songsPerformed = $db->query("SELECT * FROM play_list WHERE musicianID={$_GET['id']}");
+
+                    if ($songsPerformed->num_rows != 0) {
+                        ?>
+                        <table class="">
+                        <tr>
+                            <th>SongID</th>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Instrument Used</th>
+                        </tr>
+                        <?php
+                        while ($songPerformed = $songsPerformed->fetch_object()) {
+                            include('partials/songPerformed.php');
+                        }
+                        ?>
+                        </table>
+                    <?php
+                    } else {
+                        ?>
+                        <p>Musician does not play any songs!</p>
+                        <?php
+                    }
+                ?>
+
+                <h2>Authored Songs</h2>
+
+                <?php                 
+                    $songsAuthored = $db->query("SELECT * FROM song WHERE author={$_GET['id']}");
+
+                    if ($songsAuthored->num_rows != 0) {
+                        ?>
+                        <table class="">
+                        <tr>
+                            <th>SongID</th>
+                            <th>Title</th>
+                            <th>Album</th>
+                        </tr>
+                        <?php
+                        while ($songAuthored = $songsAuthored->fetch_object()) {
+                            include('partials/songAuthored.php');
+                        }
+                        ?>
+                        </table>
+                    <?php
+                    } else {
+                        ?>
+                        <p>Musician has not authored any songs!</p>
+                        <?php
+                    }
+                ?>
+
+                <h2>Authored Albums</h2>
                 
                 <?php require_once("partials/musician_album_wrapper.php"); ?>
                     
@@ -114,11 +164,6 @@
                 }
 
             ?>   
-            <?php
-            } catch (Exception $e) {
-                echo "<p>{$e->getMessage()}</p><a href=\"home.php\">Home</a>";
-            }
-            ?> 
     <script type="text/javascript" src="js/jquery-ui.js"></script>    <!--JQUERY - UI file for the whole website-->
     <script type="text/javascript" src="js/uiAccordion.js"></script>  <!--JQUERY - UI - Accordion file for running accordion, should be at the end-->
                 
