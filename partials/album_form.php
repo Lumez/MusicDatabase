9e13e0491c1a686bc552641a213eb2dca1d66826
gc1e12 <AARON-MAC@ug-vpn92.ecs.soton.ac.uk>
2013-12-09 21:54:25 +0000

    Update musician

--- partials/album_form.php
+++ partials/album_form.php
@@ -1,15 +1,16 @@
 
 
 <!--<input type="submit" value="Add Album"></input> -->
-
+<br/>
 <button type="button" class="btn btn-info" id="showform" onclick="$( '#album_form' ).show();$( '#showform' ).hide();">-Add Album-</button>
-
-<div class="col-md-12" id="album_form">
+<br/>
+<div class="col-md-12 page-section" id="album_form">
 	<form action='' onsubmit=''>
+				<input type="title" class="form-control" placeholder="Enter album title" name="musicianID" width="20px">
+				<input type="title" class="form-control" placeholder="Enter album title" name="title" width="20px">
+		Title: 	<input type="title" class="form-control" placeholder="Enter album title" name="title" width="20px">
 
-		<p>asdas</p>
 
-		asdas
 
 
 
