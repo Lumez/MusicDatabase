0cbce24b33f498636991a1c9db5afeda35b4b1b4
gc1e12 <AARON-MAC@ug-vpn92.ecs.soton.ac.uk>
2013-12-09 19:30:49 +0000

    UPDATE MUSICIAN.php

--- musician.php
+++ musician.php
@@ -12,6 +12,10 @@
         <?php
             try {
                 if (!isset($_GET['id']) OR empty($_GET['id'])) throw new Exception("No musician selected!");
+                
+                $albumsAuthored = $db->query("SELECT * FROM album WHERE musicianID={$_GET['id']}");
+
+                $songsPerformed = $db->query("SELECT * FROM play_list WHERE musicianID={$_GET['id']}");
 
                 $musician = $db->query("SELECT name FROM musician WHERE musicianID={$_GET['id']}");
 
@@ -23,108 +27,98 @@
                 //if there are more than 1 album, throw exception with error message saying library is empty
                 //if($songsPerformed->num_rows == 0) throw new Exception("Musician does not play any songs!");
                 //if($albumsAuthored->num_rows == 0) throw new Exception("Musician has not authored any albums!");
-            } catch (Exception $e) {
-                echo "<p>{$e->getMessage()}</p><a href=\"home.php\">Home</a>";
-            }
         ?>
                 <br />
-                <h2>Performed Songs</h2>
 
-                <?php                 
-                    $songsPerformed = $db->query("SELECT * FROM play_list WHERE musicianID={$_GET['id']}");
-
-                    if ($songsPerformed->num_rows != 0) {
-                        ?>
-                        <table class="">
-                        <tr>
-                            <th>SongID</th>
-                            <th>Title</th>
-                            <th>Author</th>
-                            <th>Instrument Used</th>
-                        </tr>
-                        <?php
-                        while ($songPerformed = $songsPerformed->fetch_object()) {
-                            include('partials/songPerformed.php');
-                        }
-                        ?>
-                        </table>
-                    <?php
-                    } else {
-                        ?>
-                        <p>Musician does not play any songs!</p>
-                        <?php
-                    }
-                ?>
-
-                <h2>Authored Songs</h2>
-
-                <?php                 
-                    $songsAuthored = $db->query("SELECT * FROM song WHERE author={$_GET['id']}");
-
-                    if ($songsAuthored->num_rows != 0) {
-                        ?>
-                        <table class="">
-                        <tr>
-                            <th>SongID</th>
-                            <th>Title</th>
-                            <th>Album</th>
-                        </tr>
-                        <?php
-                        while ($songAuthored = $songsAuthored->fetch_object()) {
-                            include('partials/songAuthored.php');
-                        }
-                        ?>
-                        </table>
-                    <?php
-                    } else {
-                        ?>
-                        <p>Musician has not authored any songs!</p>
-                        <?php
-                    }
-                ?>
-
-                <div id="accord">
-                    <div id="content">
-
-                    </div>
-                </div><!--END OF DIV "ACCORD"-->
-
-                <h2>Authored Albums</h2>
-                <?php
-                    $albumsAuthored = $db->query("SELECT * FROM album WHERE musicianID={$_GET['id']}");
-
-                    if ($albumsAuthored->num_rows != 0) {
-                    ?>
-                    <table class="">
-                        <tr>
-                            <th>AlbumID</th>
-                            <th>Title</th>
-                            <th>Date</th>
-                        </tr>
-                        <?php
-                            while ($album = $albumsAuthored->fetch_object()) {
-                                include('partials/album.php');
-                            }
-                        ?>
-                        </table>
-                    <?php
-                    } else {
-                        ?>
-                        <p>Musician has not authored any albums!</p>
-                        <?php
-                    }
-                ?>
+                <h3><u>Authored Albums</u></h3>
+                
+                <?php require_once("partials/musician_album_wrapper.php"); ?>
                     
                 <input type="submit" value="Add Album"></input>
 
             <?php    
-                if (isset($_GET['albumid']) OR empty($_GET['albumid']) == false){
+                if (isset($_GET['albumid']) OR isset($_GET['title'])){
+                    echo("<hr/>");
+                    echo("<h3>".$_GET['title']."'s song</h3>");
+                    
+                    // sql query to get all the song in the selected album
+                    $album_song = $db->query("SELECT * FROM song WHERE AlbumID={$_GET['albumid']}");
 
-                    echo("<p>album's song here</p>");
+                    if($album_song->num_rows > 0){
+            ?>
+                    <table class="">
+                        <tr>
+                            <th>No</th>
+                            <th>Title</th>
+                            <th>Song-Writer</th>
+                            <th>Instrument</th>
 
-                }else{echo("<p>Check the album's song by selecting the album above</p>");}
+                        </tr>
+            <?php
+                        $no = 1;
+                        while($song=$album_song->fetch_object()){
+                            //display the album song here            
+            ?>
+                            <tr class="targettablerow">
+                                <td><?=$no ?></td>
+                                <td><a href=''><?=$song->title?></a></td>
+
+                                <!-- get the name of the musician (author) -->
+                                <?php $musician = $db->query("SELECT name FROM musician WHERE musicianID=$song->author")->fetch_object(); ?>
+                                <td><?=$musician->name?></td>
+                                <!-- check the playlist of the songid -->
+                                <?php 
+                                    $instruments = $db->query("SELECT instrumentID FROM play_list WHERE songID=$song->songID"); 
+
+                                    if($instruments->num_rows > 0){
+                                        $instrumentplayed = "";
+
+                                        while($instrument = $instruments->fetch_object()){
+                                            $instrumentname = $db->query("SELECT name FROM instrument WHERE instrumentID=$instrument->instrumentID")->fetch_object()->name; 
+                                            
+                                                $instrumentplayed.= "$instrumentname";
+                                            
+                                        }
+                                    }else{
+                                        $instrumentplayed="none";
+                                    }
+
+                                ?>
+
+                                <td><?= $instrumentplayed ?></td>
+                            </tr>
+            <?php
+                            $no++;
+
+                        }
+            ?>
+                    </table>                   
+            <?php
+                                            
+                }else{
+                    // if there is no song in the album display no song in album
+                    echo("<p class='text-danger'><b>There are no song in the album <br/> Add a song?</b></p>");
+                }
+            ?>
+                    <hr/>
+                    <!-- add song here -->
+                    <form action="#" onsubmit="return false;" >
+                        <input type="submit" value="Add Song"/>
+
+                        <!-- form here -->
+                    </form>
+            <?php
+
+                }else{
+                    echo("<p>Check the album's song by selecting the album above</p>");
+                }
 
             ?>   
+            <?php
+            } catch (Exception $e) {
+                echo "<p>{$e->getMessage()}</p><a href=\"home.php\">Home</a>";
+            }
+            ?> 
     <script type="text/javascript" src="js/jquery-ui.js"></script>    <!--JQUERY - UI file for the whole website-->
     <script type="text/javascript" src="js/uiAccordion.js"></script>  <!--JQUERY - UI - Accordion file for running accordion, should be at the end-->
                 
