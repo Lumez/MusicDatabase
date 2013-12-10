cf03560db313761ede8a2c6c777efe85582fc9ca
gc1e12 <AARON-MAC@ug-vpn63.ecs.soton.ac.uk>
2013-12-10 20:54:18 +0000

    update musician

--- partials/album_form.php
+++ partials/album_form.php
@@ -6,9 +6,9 @@
 <br/>
 <div class="col-md-12 page-section" id="album_form">
 
-	<form role="form" action="addAlbum_process.php" method="POST" onsubmit="">
+	<form role="form" action="addAlbum_process.php" method="POST" onsubmit="return validateform();">
 		<input type="hidden" class="form-control" placeholder="Enter album title" name="musicianID" value="<?= $_GET['id']?>">
-		Title: 	<input type="title" placeholder="Enter album title" name="title" style="width:200px;">
+		Title: 	<input type="title" id="title" placeholder="Enter album title" name="title" style="width:200px;">
 
 				
 		<br/><br/> 
@@ -18,3 +18,17 @@
 
 	</form>
 </div>
+
+
+<script type="text/javascript">
+
+function validateform(){
+
+	if($("#title").value=""){
+		return false;
+		echo("please enter the title");
+	}
+	return true;
+}
+
+</script>
